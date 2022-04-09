dfe043d15c8271c2cc69be7fcb73a68cf0b0fb9b
Anna An <annaan.golf@gmail.com>
2022-04-09 12:02:22 +1200

    Update seed data

--- server/seeds/1-posts.js
+++ server/seeds/1-posts.js
@@ -1,6 +1,7 @@
 exports.seed = function (knex) {
   return knex('posts').insert([
-    { id: 1, author: 'alice', title: 'Lets watch a movie', content: 'Lets all go and watch a movie! Does anyone have any recommendations?' },
-    { id: 2, author: 'bob', title: 'Fundamentals of javascript', content: 'Javascript is a powerful programming language' },
+    { id: 1, author: 'Alice', title: 'Movie Suggestions?', content: 'Does anyone have any good movie recommendations? Comment below and I will pick one to watch later! thanks everyone!' },
+    { id: 2, author: 'Bob', title: 'Binary Search', content: `Binary search is an efficient algorithm for finding an item from a sorted list of items. It works by repeatedly dividing in half the portion of the list that could contain the item, until you've narrowed down the possible locations to just one. We used binary search in the guessing game in the introductory tutorial.
+    One of the most common ways to use binary search is to find an item in an array. For example, the Tycho-2 star catalog contains information about the brightest 2,539,913 stars in our galaxy. Suppose that you want to search the catalog for a particular star, based on the star's name. If the program examined every star in the star catalog in order starting with the first, an algorithm called linear search, the computer might have to examine all 2,539,913 stars to find the star you were looking for, in the worst case. If the catalog were sorted alphabetically by star names, binary search would not have to examine more than 22 stars, even in the worst case.` },
   ])
 }
--- server/seeds/2-comments.js
+++ server/seeds/2-comments.js
@@ -1,6 +1,18 @@
 exports.seed = function (knex) {
   return knex('comments').insert([
-    { id: 10, post_id: 1, author: 'anna', comment: 'Try watching star wars!' },
-    { id: 11, post_id: 2, author: 'alice', comment: 'javascript is slower than java' },
+    { id: 1, post_id: 1, author: 'Patty', comment: 'Star Wars!' },
+    { id: 2, post_id: 1, author: 'Peg', comment: 'Jurassic Park!' },
+    { id: 3, post_id: 1, author: 'Peg', comment: 'no watch Dune!' },
+    { id: 4, post_id: 1, author: 'Bob', comment: 'watch Parasite' },
+    { id: 5, post_id: 1, author: 'Patty', comment: 'Titanic' },
+    { id: 6, post_id: 1, author: 'Patty', comment: 'or Minari' },
+    { id: 7, post_id: 1, author: 'Patty', comment: 'Forrest Gump!!!' },
+    { id: 8, post_id: 2, author: 'Alice', comment: 'Binary search is awesome ;)' },
+    { id: 9, post_id: 2, author: 'Alice', comment: 'Some more info... In computer science, binary search, also known as half-interval search, logarithmic search, or binary chop, is a search algorithm that finds the position of a target value within a sorted array. Binary search compares the target value to the middle element of the array.' },
+    { id: 10, post_id: 2, author: 'Alice', comment: 'Binary search works on sorted arrays. Binary search begins by comparing an element in the middle of the array with the target value. If the target value matches the element, its position in the array is returned. If the target value is less than the element, the search continues in the lower half of the array. If the target value is greater than the element, the search continues in the upper half of the array. By doing this, the algorithm eliminates the half in which the target value cannot lie in each iteration.' },
+    { id: 11, post_id: 2, author: 'Bob', comment: 'Binary chop! I\'ve never heard it being called binary chop... That\'s so funny' },
+    { id: 12, post_id: 2, author: 'Alice', comment: 'I know right! It\'s not from me, I just copy and pasted from wikipedia...' },
+    { id: 13, post_id: 2, author: 'Alice', comment: 'I\'ve run out of comment ideas now' },
+    { id: 14, post_id: 2, author: 'Alice', comment: 'I guess I\'ll just put random stuff...' },
   ])
 }
