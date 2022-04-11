3da5796ae27ffa63839fd269042bcde30318c33d
Anna An <annaan.golf@gmail.com>
2022-04-11 22:38:42 +1200

    Update KMP algorithm and styling

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import PostDisplay from "./PostDisplay";
 import { Link } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Posts = ({ posts, searchKey, handleSearch }) => {
   return (
     <div className="posts">
       <Link to="/posts/new"><button className="btn">New Post</button></Link>
-      <input type="text" value={searchKey} onChange={handleSearch}/>
+      <span className="search-bar">
+        <input type="text" value={searchKey} onChange={handleSearch} className="search-input"/>
+        <SearchIcon className="search-icon"/>
+      </span>
       {posts.map((post) => <PostDisplay key={'post' + post.id} postId={post.id} title={post.title}/>)}
     </div>
   )
--- client/src/functions.js
+++ client/src/functions.js
@@ -11,7 +11,7 @@ function kmp(str, substr) {
       i++;
       if (i === substr.length) return true;
     } else {
-      if (i !== 0) i = lps[i];
+      if (i !== 0) i = lps[i - 1];
     }
   }
   return false;
@@ -23,7 +23,7 @@ function getLPS(str) {
     if (str[i] === str[j]) {
       lps[j] = ++i;
     } else {
-      if (i !== 0) i = lps[i];
+      if (i !== 0) i = lps[i - 1];
     }
   }
   return lps;
