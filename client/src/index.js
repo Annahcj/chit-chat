50c9ed5a5e8847a16b4a8545b2d015cf7c83dc12
Anna An <annaan.golf@gmail.com>
2022-04-16 22:52:47 +1200

    Update ReactDOM.render to createRoot for React 18

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
