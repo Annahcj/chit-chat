0ed84f0d639517fbeee7102972feef06aa469154
Anna An <annaan.golf@gmail.com>
2022-04-16 22:57:46 +1200

    Switch back to using ReactDOM.render

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container);
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
