194e2c2f0311d00aa470006a68cfcf17810821e1
Anna An <annaan.golf@gmail.com>
2022-04-18 18:37:30 +1200

    Configure db.js

--- server/db.js
+++ server/db.js
@@ -38,12 +38,12 @@ function getPostsAndComments(db = connection) {
 
 function insertComment(post_id, author, comment, db = connection) {
   return db('comments')
-    .insert({ post_id, author, comment, created_at: new Date() })
+    .insert({ post_id, author, comment, created_at: new Date() }, 'id')
 }
 
 function insertPost(author, title, content, db = connection) {
   return db('posts')
-    .insert({ author, title, content, created_at: new Date() })
+    .insert({ author, title, content, created_at: new Date() }, 'id')
 }
 
 function getPostById(id, db = connection) {
