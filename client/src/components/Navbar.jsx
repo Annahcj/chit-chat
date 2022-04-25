f323d5dd13b0a983b9c4500de7506a3f76a277da
Anna An <annaan.golf@gmail.com>
2022-04-26 11:56:29 +1200

    Use useAuth0 hook and add conditional rendering of auth buttons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const Navbar = () => {
+  const { isAuthenticated } = useAuth0();
+  
   return (
     <div className="nav">
       <h1>ChitChat</h1>
       <Link to="/">
         <button className="btn">Home</button>
       </Link>
+      {isAuthenticated ? (
+        <button className="btn logout-button">Logout</button>
+      ) : (
+        <>
+          <button className="btn login-button">Login</button>
+          <button className="btn signup-button">Sign Up</button>
+        </>
+      )}
     </div>
   )
 }
