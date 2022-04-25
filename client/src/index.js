b3d4e902631f18c7ffd1d188d8b78a4ea7d9c1d0
Anna An <annaan.golf@gmail.com>
2022-04-26 11:40:56 +1200

    Add Auth0Provider

--- client/src/index.js
+++ client/src/index.js
@@ -4,20 +4,28 @@ import './index.css'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-import { reducers } from './state/reducers/index.js';
+import { Auth0Provider } from '@auth0/auth0-react'
+
+import { reducers } from './state/reducers/index.js'
 
 const store = createStore(reducers, {}, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <Auth0Provider
+      domain="whai-2022-anna.au.auth0.com"
+      clientId="KPeD8i6N1ofkYoAneDEs7nBQ6SfMbK4c"
+      audience="https://api"
+    >
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
