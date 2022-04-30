5cbe306b24c83da9c2fd01d55b959961494ab5ab
Anna An <annaan.golf@gmail.com>
2022-05-01 11:17:56 +1200

    Switch back to past commit

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
   }, [dispatch])
 
   useEffect(() => {
-    setDisplayPosts(posts)
+    setDisplayPosts([...posts].reverse())
   }, [posts])
 
   // Search function
@@ -35,7 +35,7 @@ function App() {
     setDisplayPosts(
       posts.filter((post) => {
         return hasMatch(post.title.toLowerCase(), e.target.value)
-      })
+      }).reverse()
     )
   }
 
